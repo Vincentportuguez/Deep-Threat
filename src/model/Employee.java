6da2f2ce6b4f9cc39166bf6062b2b8ef1dc9e056
Caren VDP <vdpcaren17@gmail.com>
2017-10-02 08:48:41 +0300

    Update Employee.java

--- src/model/Employee.java
+++ src/model/Employee.java
@@ -4,8 +4,82 @@ public class Employee {
 
 	private String username;
 	private String password;
+	private String empName;
+	private int Id;
+	private int BasicPay;
+	private int allowance;
+	private int sss;
+	private int overTime;
+	private int pHealth;
+	private int HDMF;
+	private int tax;
+	
+	public String getEmpName(){
+		return empName;
+	}
+	
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+
+	public void setId(int id) {
+		Id = id;
+	}
+
+	public void setBasicPay(int basicPay) {
+		BasicPay = basicPay;
+	}
+
+	public void setAllowance(int allowance) {
+		this.allowance = allowance;
+	}
+
+	public void setSss(int sss) {
+		this.sss = sss;
+	}
+
+	public void setOverTime(int overTime) {
+		this.overTime = overTime;
+	}
+
+	public void setpHealth(int pHealth) {
+		this.pHealth = pHealth;
+	}
+
+	public void setHDMF(int hDMF) {
+		HDMF = hDMF;
+	}
+
+	public void setTax(int tax) {
+		this.tax = tax;
+	}
+
+	public int getId() {
+		return Id;
+	}
+	public int getBasicPay(){
+		return BasicPay;
+	}
+	public int getAllowance() {
+		return allowance;
+	}
+	public int getSSS() {
+		return sss;
+	}
+	public int getOverTime() {
+		return overTime;
+	}
+	public int getPhealth() {
+		return pHealth;
+	}
 	public String getUsername() {
 		return username;
+	}
+	public int getHDMF() {
+		return HDMF;
+	}
+	public int getTax() {
+		return tax;
 	}
 	public void setUsername(String username) {
 		this.username = username;
